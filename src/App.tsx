81f0972097ae3d6dc22aed06a6fe1723dc1bd8b0
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-02-20 21:11:12 +0900

    [chore] #noissue : shadcn 라이브러리 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
-  return <div className=" bg-red-700 w-[150px] h-[150px]">test</div>;
+  return (
+    <div>
+      <Button className="bg-red-500 cursor-alias">Click me</Button>
+    </div>
+  );
 }
 
 export default App;
