2ceb52cb8a404f382220adfd999b41dcf19c09ad
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-02-22 22:29:03 +0900

    [refactor] #noissue : 중복되는 코드 수정

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,9 +1,4 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { AppProviders } from "./providers/index.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <AppProviders>
-    <App />
-  </AppProviders>
-);
+createRoot(document.getElementById("root")!).render(<App />);
