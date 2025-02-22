4b9d5f2458354c365fa49672baf6bebbf3f6f04d
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-02-22 15:11:17 +0900

    [feat] #noissue : 라우터 설정

---
+++ src/app/routes/routes.tsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import LoginPage from "@/pages/LoginPage";
+import MainPage from "@/pages/mainPage";
+import Mypage from "@/pages/myPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/", // 메인 페이지 보호
+    element: "",
+    children: [
+      {
+        index: true,
+        element: <MainPage />,
+      },
+      {
+        path: "mypage/:userId",
+        element: <Mypage />,
+      },
+    ],
+  },
+]);
+
+export default router;
