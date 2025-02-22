c8e8af2c7d80c36ee989e3b1af205143ab82853d
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-02-22 22:21:21 +0900

    [chore] #noissue : 필요없는 코드 제거

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,12 +1,9 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { AppProviders } from "./providers/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>  
-    <AppProviders>
-      <App />
-    </AppProviders>
-  </StrictMode>
+  <AppProviders>
+    <App />
+  </AppProviders>
 );
