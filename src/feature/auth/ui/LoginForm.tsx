1f195ee0334ebdf87020751ee3e59c9616497501
RohSungKyun <songkyun1108@naver.com>
2025-02-24 15:33:34 +0900

    [feat] #13 : 클라이언트 validation 구현

--- src/feature/auth/ui/LoginForm.tsx
+++ src/feature/auth/ui/LoginForm.tsx
@@ -2,8 +2,38 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import PasswordChangeModal from "./PasswordChangeModal";
+import React, { useState } from "react";
+import { validatePassword } from "../../../../shared/utils/validation";
 
 function LoginForm() {
+  const [employeeId, setEmployeId] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [IsIdValid, setIsIdValid] = useState(true);
+  const [IsPasswordValid, setIsPasswordValid] = useState(true);
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newPassword = e.target.value;
+    setPassword(newPassword);
+
+    const validation = validatePassword(newPassword);
+    if(!validation.valid) {
+      setErrorMessage(validation.message || "비밀번호가 유효하지 않습니다.");
+      setIsPasswordValid(false);
+    } else {
+      setErrorMessage(""); // 올바른 형식으로 입력
+      setIsPasswordValid(true);
+    }
+  }
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      alert("로그인 성공!"); // 실제 로그인 로직 실행 예정, 이후 메인으로
+    }
+  }
+
   return(
     <div className="w-full max-w-md">
       <div className="flex flex-col items-center mb-4">
@@ -15,12 +45,26 @@ function LoginForm() {
       </div>
       <Card className="h-[300px] flex flex-col justify-center">
         <CardContent>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="mb-4">
-              <Input id="emplyeeId" type="text" placeholder="사번을 입력해주세요" />
+              <Input
+                id="emplyeeId"
+                type="text"
+                placeholder="사번을 입력해주세요"
+                value={employeeId} onChange={(e) => setEmployeId(e.target.value)}
+              />
             </div>
             <div className="mb-4">
-              <Input id="password" type="password" placeholder="비밀번호를 입력해주세요." />
+              <Input
+                id="password"
+                type="password"
+                placeholder="비밀번호를 입력해주세요."
+                value={password} onChange={handlePasswordChange}
+                valid={IsPasswordValid}
+              />
+              {errorMessage && (
+                <p className="text-red-400 text-sm mb-2">{errorMessage}</p>
+              )}
             </div>
             <Button type="submit" className="w-full bg-gray-800 hover:bg-gray-600">
               로그인
@@ -28,7 +72,7 @@ function LoginForm() {
           </form>
           <div className="flex text-center mt-4 justify-center items-center">
             <p className="text-sm text-gray-800">
-              비밀번호를 잊어버리셨나요? 
+              비밀번호를 잊어버리셨나요?
             </p>
             <PasswordChangeModal />
           </div>
