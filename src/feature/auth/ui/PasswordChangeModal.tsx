276651b8f0b6e6acfdae9bfc9274dd47690f10cd
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-02-25 19:12:02 +0900

    [feat] #17 : 인풋 validation 로직 구현

--- src/feature/auth/ui/PasswordChangeModal.tsx
+++ src/feature/auth/ui/PasswordChangeModal.tsx
@@ -9,26 +9,40 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { ChangeEvent, MouseEvent, useEffect, useState } from "react";
+import { ChangeEvent, FormEvent, MouseEvent, useEffect, useRef, useState } from "react";
+import { validatePassword } from "../../../../shared/utils/validation";
 
 function PasswordChangeModal() {
   const [formData, setFormData] = useState({
-    userId: "12345678",
+    userId: "",
+    email: "",
+    authNumber: "",
+    password: "",
+    rePassword: "",
+  });
+  const [success, setSuccess] = useState({
+    email: "",
+    authNumber: "",
+  });
+  const [errors, setErrors] = useState({
+    userId: "",
     email: "",
     authNumber: "",
     password: "",
     rePassword: "",
   });
   const [isFormValid, setIsFormValid] = useState(false);
+  const authNumberRef = useRef<HTMLInputElement | null>(null);
 
   // 모든 필드가 채워졌는지 확인
   useEffect(() => {
     const isValid =
-      Object.values(formData).every(value => value.trim() !== "") && // 모든 필드가 비어있지 않은지 확인
-      formData.password === formData.rePassword; // 비밀번호 확인
+      Object.values(formData).every(value => value.trim() !== "") &&
+      formData.password === formData.rePassword &&
+      Object.values(errors).every(error => error === "");
 
     setIsFormValid(isValid);
-  }, [formData]);
+  }, [formData, errors]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { id, value } = event.target;
@@ -36,14 +50,78 @@ function PasswordChangeModal() {
       ...prevData,
       [id]: value,
     }));
+    setErrors(prevErrors => ({
+      ...prevErrors,
+      [id]: "",
+    }));
+
+    if (id === "password") {
+      const validation = validatePassword(value);
+      if (!validation.valid) {
+        setErrors(prevErrors => ({
+          ...prevErrors,
+          password: "비밀번호는 8~16자이며, 대문자, 소문자, 숫자, 특수문자를 포함해야 합니다.",
+        }));
+      } else {
+        setErrors(prevErrors => ({
+          ...prevErrors,
+          password: "",
+        }));
+      }
+    }
+
+    if (id === "rePassword") {
+      if (formData.password !== value) {
+        setErrors(prevErrors => ({
+          ...prevErrors,
+          rePassword: "비밀번호가 일치하지 않습니다.",
+        }));
+      } else {
+        setErrors(prevErrors => ({
+          ...prevErrors,
+          rePassword: "",
+        }));
+      }
+    }
   };
 
+  // 제출 핸들러
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+  };
+
+  // 인증 이메일 요청
   const handleEmailVerificationRequest = (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
+
+    // api 요청 후 리스폰스에 따라 disabled 처리 여부를 결정하는 로직 추가해야함
+    setSuccess(prevState => ({
+      ...prevState, // 기존 상태 유지
+      email: "메일로 발송된 인증번호를 확인해주세요.",
+    }));
+    setErrors(prevState => ({
+      ...prevState, // 기존 상태 유지
+      email: "존재하지 않는 메일입니다.",
+    }));
+    if (authNumberRef.current) {
+      authNumberRef.current.disabled = false;
+      authNumberRef.current.focus();
+    }
   };
 
+  // 인증 번호 확인
   const handleAuthCodeVerification = (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
+
+    // api 요청 후 리스폰스에 따라 disabled 처리 여부를 결정하는 로직 추가해야함
+    setSuccess(prevState => ({
+      ...prevState, // 기존 상태 유지
+      authNumber: "인증되었습니다.",
+    }));
+    setErrors(prevState => ({
+      ...prevState, // 기존 상태 유지
+      authNumber: "인증번호가 틀렸습니다.",
+    }));
   };
 
   return (
@@ -55,15 +133,21 @@ function PasswordChangeModal() {
         <DialogHeader>
           <DialogTitle>비밀번호 변경</DialogTitle>
         </DialogHeader>
-        <form className="grid gap-4 py-6">
+        <form className="grid gap-4 py-6" onSubmit={handleSubmit}>
           {/* 사번 */}
           <div className="grid grid-cols-5 gap-4">
             <Label htmlFor="userId" className="text-right pt-3">
               사번
             </Label>
             <div className="col-span-3 flex flex-col">
-              <Input id="userId" value={formData.userId} onChange={handleChange} className="col-span-3" />
-              <p className="text-red-400 text-[12px] mt-1">비밀번호 틀림</p>
+              <Input
+                id="userId"
+                value={formData.userId}
+                onChange={handleChange}
+                className="col-span-3"
+                placeholder="사번을 입력해주세요."
+              />
+              {errors.userId && <p className="text-red-400 text-[12px] mt-1">{errors.userId}</p>}
             </div>
           </div>
 
@@ -73,8 +157,15 @@ function PasswordChangeModal() {
               인증 이메일
             </Label>
             <div className="col-span-3 flex flex-col">
-              <Input id="email" value={formData.email} onChange={handleChange} className="col-span-3" />
-              <p className="text-red-400 text-[12px] mt-1">비밀번호 틀림</p>
+              <Input
+                id="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="col-span-3"
+                placeholder="인증번호를 받으실 이메일 주소를 입력해주세요."
+              />
+              {success.email && <p className="text-green-400 text-[12px] mt-1">{success.email}</p>}
+              {errors.email && <p className="text-red-400 text-[12px] mt-1">{errors.email}</p>}
             </div>
             <Button type="button" onClick={handleEmailVerificationRequest}>
               요청
@@ -88,13 +179,16 @@ function PasswordChangeModal() {
             </Label>
             <div className="col-span-3 flex flex-col">
               <Input
+                ref={authNumberRef}
                 type="tel"
                 id="authNumber"
                 value={formData.authNumber}
                 onChange={handleChange}
                 className="col-span-3"
+                disabled={true}
               />
-              <p className="text-red-400 text-[12px] mt-1">비밀번호 틀림</p>
+              {success.authNumber && <p className="text-green-400 text-[12px] mt-1">{success.authNumber}</p>}
+              {errors.authNumber && <p className="text-red-400 text-[12px] mt-1">{errors.authNumber}</p>}
             </div>
             <Button type="button" onClick={handleAuthCodeVerification}>
               확인
@@ -107,23 +201,33 @@ function PasswordChangeModal() {
               새 비밀번호
             </Label>
             <div className="col-span-3 flex flex-col">
-              <Input type="password" id="password" value={formData.password} onChange={handleChange} />
-              <p className="text-red-400 text-[12px] mt-1">비밀번호 틀림</p>
+              <Input
+                type="password"
+                id="password"
+                value={formData.password}
+                onChange={handleChange}
+                placeholder="새 비밀번호를 입력해주세요"
+              />
+              {errors.password && <p className="text-red-400 text-[12px] mt-1">{errors.password}</p>}
             </div>
           </div>
 
           {/* 새 비밀번호 확인 */}
-          <div className="grid grid-cols-5 items-center gap-4">
-            <Label htmlFor="rePassword" className="text-right whitespace-nowrap">
+          <div className="grid grid-cols-5 gap-4">
+            <Label htmlFor="rePassword" className="text-right whitespace-nowrap pt-3">
               새 비밀번호 확인
             </Label>
-            <Input
-              type="password"
-              id="rePassword"
-              value={formData.rePassword}
-              onChange={handleChange}
-              className="col-span-3"
-            />
+            <div className="col-span-3 flex flex-col">
+              <Input
+                type="password"
+                id="rePassword"
+                value={formData.rePassword}
+                onChange={handleChange}
+                className="col-span-3"
+                placeholder="새 비밀번호를 한 번 더 입력해주세요."
+              />
+              {errors.rePassword && <p className="text-red-400 text-[12px] mt-1">{errors.rePassword}</p>}
+            </div>
           </div>
 
           {/* 변경하기 버튼 */}
