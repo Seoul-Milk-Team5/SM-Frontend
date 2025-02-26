c3387934e61bebc8488386893edd341fd178c037
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-02-26 17:29:12 +0900

    [design] #17 : title 컴포넌트 분리 후 디자인 적용

--- src/pages/PasswordChangePage.tsx
+++ src/pages/PasswordChangePage.tsx
@@ -1,10 +1,11 @@
 import Description from "@/feature/passwordChange/ui/Description";
 import PasswordChangeForm from "@/feature/passwordChange/ui/PasswordChangeForm";
+import Title from "@/feature/passwordChange/ui/Title";
 
 function PasswordChangePage() {
   return (
     <main className="max-w-[1280px] mx-auto min-h-screen flex flex-col justify-center items-center gap-6">
-      <h1 className="w-[50%] text-xl font-bold">비밀번호 변경</h1>
+      <Title />
       <PasswordChangeForm />
       <Description />
     </main>
