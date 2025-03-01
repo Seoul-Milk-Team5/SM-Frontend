ac49376dd03cb316ed029de32c05cad3eb920ea1
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-03-01 21:28:13 +0900

    [design] #28 : 데이터 테이블 디자인 적용 및 메인페이지 디자인 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,7 @@
 import Navbar from "../../shared/ui/Navbar";
 import FileDndBox from "../../shared/ui/FileDndBox";
+import { FileUploadTable } from "@/feature/main/ui/FileUploadTable";
+import { Button } from "@/components/ui/button";
 
 function MainPage() {
   const navItems = [
@@ -9,9 +11,25 @@ function MainPage() {
 
   return (
     <main>
-      <Navbar items={navItems} />
+      <div className="flex justify-between items-center">
+        <Navbar items={navItems} />
+        <div className="flex items-center gap-3.5 mb-10">
+          <Button
+            variant="outline"
+            className="text-green-500 border-green-500 hover:text-green-600 cursor-pointer disabled:opacity-100 py-3.5 px-6 text-[17px] disabled:border-gray-100 disabled:text-gray-300"
+            disabled={true}>
+            임시 저장
+          </Button>
+          <Button
+            className="bg-green-500 text-white hover:bg-green-600 cursor-pointer disabled:bg-green-200 disabled:opacity-100 py-3.5 px-6 text-[17px]"
+            disabled={true}>
+            검사하기
+          </Button>
+        </div>
+      </div>
       <FileDndBox />
-      <div className="w-full bg-gray-0 h-1.5"></div>
+      <div className="w-full bg-gray-0 h-1.5 mb-13"></div>
+      <FileUploadTable />
     </main>
   );
 }
