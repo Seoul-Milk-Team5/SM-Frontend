7f92602e0fde3148c3314a484c1d2fb53daf4b5b
RohSungKyun <songkyun1108@naver.com>
2025-03-02 21:42:52 +0900

    [feat] #26 : re03 UI 구현

--- src/feature/searchFile/model/mock/approvalData.tsx
+++ src/feature/searchFile/model/mock/approvalData.tsx
@@ -1,16 +1,16 @@
 import { ApprovalData } from "..";
 
 export const approvalData: ApprovalData = {
-    id: "001",
-    provider: "서울우유 강동지점",
-    approvalNumber: "20250228-22221644-47877741",
-    date: "25.02.28",
-    providerName: "서울우유 강동지점",
-    providerReg: "859-81-02258",
-    receiverName: "파리바게트 서울역점",
-    receiverReg: "859-81-02258",
-    supplyAmount: "30,888,444",
-    taxAmount: "5,812,694",
-    totalAmount: "36,701,138",
-    statusMessage: "홈택스 조회 결과 발급된 사실이 있습니다.",
-  };
+  id: "001",
+  provider: "서울우유 강동지점",
+  approvalNumber: "20250228-22221644-47877741",
+  date: "25.02.28",
+  providerName: "서울우유 강동지점",
+  providerReg: "859-81-02258",
+  receiverName: "파리바게트 서울역점",
+  receiverReg: "859-81-02258",
+  supplyAmount: "30,888,444",
+  taxAmount: "5,812,694",
+  totalAmount: "36,701,138",
+  statusMessage: "홈택스 조회 결과 발급된 사실이 있습니다.",
+};
