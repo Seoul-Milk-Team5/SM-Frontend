daa04a09f469a93529a351ee5c1e3a96290ce1cd
RohSungKyun <songkyun1108@naver.com>
2025-03-03 15:04:00 +0900

    [fix] #36: url변경에 의한 사이드바 조정

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,7 +1,11 @@
 // 일반 사원이 사용하는 페이지
 
 function UserPage() {
-  return <div>UserPage</div>
+  return (
+    <div className="bg-[#FFF] rounded-[10px] ">
+      <div className="w-[78px] mb-10 border-b-3 border-gray-800  text-gray-800 text-title-md">업무 조회</div>
+    </div>
+  )
 }
 
 export default UserPage;
