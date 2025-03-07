ae51e4379d4468e54812c223bed025aad3257e4d
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-03-07 15:54:25 +0900

    [feat] #44 : 모바일 버전 화면 테스트

--- src/pages/MobileMainPage.tsx
+++ src/pages/MobileMainPage.tsx
@@ -1,5 +1,50 @@
+import { useFileContext } from "@/app/providers/FileProvider";
+import { Button } from "@/components/ui/button";
+import { ChangeEvent, useRef } from "react";
+
 function MobileMainPage() {
-  return <>MobileMainPage</>;
+  const { mergeFiles, updateClientFiles } = useFileContext();
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files) return;
+    const selectedFiles = Array.from(event.target.files);
+    updateClientFiles(selectedFiles);
+  };
+
+  const handleButtonClick = () => {
+    fileInputRef.current?.click();
+  };
+  return (
+    <>
+      <div className="w-full flex justify-between items-center border-b-2 border-b-gray-50 py-5">
+        <h1 className="text-[#000] text-title-md-b">사진 업로드</h1>
+        <div className="flex items-center">
+          <img className="w-[45px] h-[45px] cursor-pointer gap-3" src="/icon/camera.svg" alt="휴대폰 카메라 켜기" />
+          <Button className="w-[58px] h-[30px]">선택</Button>
+        </div>
+      </div>
+      <section>
+        <div>
+          <input
+            type="file"
+            ref={fileInputRef}
+            accept="image/*"
+            className="hidden"
+            multiple
+            onChange={handleFileChange}
+          />
+          <button
+            type="button"
+            className="bg-green-500 hover:bg-green-600 disabled:bg-gray-100 text-white rounded-[10px] text-body-lg h-[50px] w-[300px] cursor-pointer"
+            disabled={mergeFiles.length >= 50}
+            onClick={handleButtonClick}>
+            파일 선택
+          </button>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default MobileMainPage;
