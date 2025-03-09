a778280c966d85c64992d79e0b31fd4f7ff4f394
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-03-09 13:19:18 +0900

    [feat] #48 : email 인증 API 로직 작성

--- src/feature/passwordChange/model/PasswordChange.type.ts
+++ src/feature/passwordChange/model/PasswordChange.type.ts
@@ -15,3 +15,17 @@ export interface IsButtonDisabled {
   email: boolean;
   authNumber: boolean;
 }
+
+export interface EmailPostResponse {
+  code: number | string;
+  message: string;
+  result: { errorClass: string; errorMessage: string } | string;
+  success: boolean;
+}
+
+export interface EmailVerificationResponse {
+  code: number | string;
+  message: string;
+  result: { errorClass: string; errorMessage: string } | string;
+  success: boolean;
+}
---
+++ src/feature/passwordChange/service/emailRequest.ts
@@ -0,0 +1,27 @@
+import { getFetchHeader } from "@/shared/utils";
+import { EmailPostResponse } from "../model";
+import { baseHttpClient } from "@/shared/api";
+
+export async function emailPostRequest(token: string, body: string): Promise<EmailPostResponse> {
+  const HEADER = getFetchHeader(token, "a");
+  console.log(body);
+
+  try {
+    return await baseHttpClient().post<EmailPostResponse, string>("api/emails/verifiation-requests", HEADER, body);
+  } catch (error) {
+    console.error("Fetch error:", error);
+    throw error;
+  }
+}
+
+export async function emailVerificationRequest(token: string, body: string): Promise<EmailPostResponse> {
+  const HEADER = getFetchHeader(token, "a");
+  console.log(body);
+
+  try {
+    return await baseHttpClient().post<EmailPostResponse, string>("api/emails/verifiations", HEADER, body);
+  } catch (error) {
+    console.error("Fetch error:", error);
+    throw error;
+  }
+}
