292b2f0742063741cf2e371e5560834e94b9917b
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-03-12 21:53:24 +0900

    [fix] #noissue : 임시저장 삭제 수정

--- src/feature/main/service/SaveFileRequest.ts
+++ src/feature/main/service/SaveFileRequest.ts
@@ -1,6 +1,6 @@
 import { baseHttpClient } from "@/shared/api";
 import { getFetchHeader } from "@/shared/utils";
-import { SaveFileGetResponse, SaveFilePacthResponse, SaveFilePostResponse } from "../model";
+import { SaveFileGetResponse, SaveFilePostResponse } from "../model";
 
 export async function saveFileGetRequest(token: string): Promise<SaveFileGetResponse> {
   const HEADER = getFetchHeader(token, "a");
@@ -33,17 +33,15 @@ export async function saveFilePostRequest(token: string, files: File[]): Promise
   }
 }
 
-export async function saveFilePatchRequest(token: string, iamgeId: number[]): Promise<SaveFilePostResponse> {
+export async function saveFilePatchRequest(token: string, imageIds: number[]): Promise<SaveFilePostResponse> {
   const HEADER = getFetchHeader(token, "a");
-  const imageIdObject = {
-    imageIds: iamgeId,
-  };
+
+  // imageIds를 쿼리 스트링으로 변환
+  const queryString = imageIds.map(id => `imageIds=${id}`).join("&");
+  const url = `api/image/tmp/unmark?${queryString}`;
+
   try {
-    return await baseHttpClient().patch<SaveFilePacthResponse, { imageIds: number[] }>(
-      "api/image/tmp/unmark",
-      HEADER,
-      imageIdObject
-    );
+    return await baseHttpClient().patch<SaveFilePostResponse, string>(url, HEADER, "");
   } catch (error) {
     console.error("Fetch error:", error);
     throw error;
