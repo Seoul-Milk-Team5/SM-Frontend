c8d8c5acc3abd90643c5a81e5b0447c1f7d69799
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-03-13 17:15:20 +0900

    [fix] : 로그인 상태에서 메인페이지 접속시 사이듣바에 유저정보 표시 안되는 오류 해결

--- src/feature/my/ui/MyModal.tsx
+++ src/feature/my/ui/MyModal.tsx
@@ -2,14 +2,28 @@ import { useStep } from "@/app/providers/StepProvider";
 import { Dialog, DialogContent, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import PasswordCheckContent from "./PasswordCheckContent";
 import UserContent from "./UserContent";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "@/app/providers/AuthProvider";
+import { userInformationRequest } from "../service";
 
 function myModal() {
   const { steps } = useStep();
-  const { getUserData } = useAuth();
+  const { getUser, getUserData } = useAuth();
   const [isModalState, setIsModalState] = useState(false);
+  const [userState, setUserState] = useState({
+    userName: "",
+    userId: "",
+  });
   const userData = getUserData();
+
+  useEffect(() => {
+    if (userData.userId === "" || userData.userName === "") {
+      const token = getUser();
+      userInformationRequest(token).then(result =>
+        setUserState(prev => ({ ...prev, userName: result.result.name, userId: result.result.employeeId }))
+      );
+    }
+  }, []);
 
   const maxWidthClass = {
     1: "sm:max-w-[750px] max-h-[420px]",
@@ -21,8 +35,8 @@ function myModal() {
       <DialogTrigger asChild>
         <div className="hover:bg-green-0 flex justify-between items-center cursor-pointer">
           <div>
-            <p className="text-body-md">{userData.userName}</p>
-            <p className="text-label-xs">{userData.userId}</p>
+            <p className="text-body-md">{userData.userName || userState.userName}</p>
+            <p className="text-label-xs">{userData.userId || userState.userId}</p>
           </div>
           <img className="w-[24px]" src="/icon/gear.svg" alt="마이페이지" />
         </div>
--- src/pages/MyPage.tsx
+++
@@ -1,7 +0,0 @@
-// 실질적으로 사용되지 않음.
-
-function Mypage() {
-  return <div>mypage</div>;
-}
-
-export default Mypage;
